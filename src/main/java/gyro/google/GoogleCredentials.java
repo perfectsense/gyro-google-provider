0ffe27bb87d67c5c53700dde64ffb85e9ee7a31a
Jeremy Collins <jeremy@perfectsensedigital.com>
2019-05-07 11:28:49 -0400

    Update GoogleCredentials class to implement generic

--- src/main/java/gyro/google/GoogleCredentials.java
+++ src/main/java/gyro/google/GoogleCredentials.java
@@ -1,13 +1,19 @@
 package gyro.google;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import gyro.core.resource.ResourceName;
 import gyro.core.Credentials;
 import com.google.common.collect.ImmutableMap;
+import gyro.core.scope.FileScope;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.Map;
 
 @ResourceName("credentials")
-public class GoogleCredentials extends Credentials {
+public class GoogleCredentials extends Credentials<GoogleCredential> {
 
     private String projectId;
 
@@ -35,15 +41,25 @@ public class GoogleCredentials extends Credentials {
     }
 
     @Override
-    public Map<String, String> findCredentials(boolean refresh) {
+    public GoogleCredential findCredentials(boolean refresh) {
         return findCredentials(refresh, true);
     }
 
     @Override
-    public Map<String, String> findCredentials(boolean refresh, boolean extended) {
-        ImmutableMap.Builder<String, String> mapBuilder = new ImmutableMap.Builder<>();
+    public GoogleCredential findCredentials(boolean refresh, boolean extended) {
+        try {
+            return GoogleCredential.fromStream(getRelativeCredentialsPath())
+                .createScoped(Collections.singleton("https://www.googleapis.com/auth/cloud-platform"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        return mapBuilder.build();
+        return null;
     }
 
+    private InputStream getRelativeCredentialsPath() throws Exception {
+        FileScope fileScope = scope().getFileScope();
+
+        return fileScope.getRootScope().getBackend().openInput(Paths.get(fileScope.getFile()).getParent().resolve(getCredentialFilePath()).toString());
+    }
 }
