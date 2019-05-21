a58d3539ea22d526b6224652cf4cb8b9b2e80dce
Hyoo Lim <hyoolim@gmail.com>
2019-05-21 14:03:37 -0400

    Moves createClient logic into GoogleCredentials.

--- src/main/java/gyro/google/GoogleResource.java
+++ src/main/java/gyro/google/GoogleResource.java
@@ -1,68 +1,16 @@
 package gyro.google;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.googleapis.services.json.AbstractGoogleJsonClient;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.compute.Compute;
-import gyro.core.GyroException;
 import gyro.core.resource.Resource;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.Collections;
-
 public abstract class GoogleResource extends Resource {
-    private String projectId;
 
     protected <T extends AbstractGoogleJsonClient> T creatClient(Class<T> clientClass) {
-
-        if (clientClass.getSimpleName().equals("Compute")) {
-            return (T) getClient();
-        }
-
-        return null;
-    }
-
-    private GoogleCredential getCredentials() {
-        try {
-            gyro.google.GoogleCredentials credentials = (gyro.google.GoogleCredentials) credentials();
-
-            return GoogleCredential.fromStream(
-                new FileInputStream(credentials.getCredentialFilePath())
-            ).createScoped(Collections.singleton("https://www.googleapis.com/auth/cloud-platform"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    private Compute getClient() {
-        try {
-            gyro.google.GoogleCredentials credentials = (gyro.google.GoogleCredentials) credentials();
-
-            HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-            JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-
-            return new Compute.Builder(httpTransport, jsonFactory, credentials.findCredentials(true))
-                .setApplicationName("gyro-google-provider")
-                .build();
-        } catch (GeneralSecurityException | IOException e) {
-            throw new GyroException("Unable to create Compute client");
-        }
+        return ((GoogleCredentials) credentials()).createClient(clientClass);
     }
 
     protected String getProjectId() {
-        if (projectId == null) {
-            gyro.google.GoogleCredentials credentials = (gyro.google.GoogleCredentials) credentials();
-            projectId = credentials.getProjectId();
-        }
-
-        return projectId;
+        return ((GoogleCredentials) credentials()).getProjectId();
     }
 
 }
