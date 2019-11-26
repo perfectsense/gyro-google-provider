c157338a8c19b97b0811bc99e35dbed8262f0b77
Kevin Luman <kevinleeluman@gmail.com>
2019-11-26 16:10:55 -0500

    Adds BucketAccessControlResource and BucketAccessControlFinder implementations.

--- src/main/java/gyro/google/storage/BucketAccessControlFinder.java
+++ src/main/java/gyro/google/storage/BucketAccessControlFinder.java
@@ -16,5 +16,112 @@
 
 package gyro.google.storage;
 
-public class BucketAccessControlResourceFinder {
+import com.google.api.services.storage.Storage;
+import com.google.api.services.storage.model.Bucket;
+import com.google.api.services.storage.model.BucketAccessControl;
+import com.google.api.services.storage.model.Buckets;
+import gyro.core.GyroException;
+import gyro.core.Type;
+import gyro.google.GoogleFinder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Query for Bucket Access Controls.
+ *
+ * ========
+ * Examples
+ * ========
+ *
+ * **Find all Bucket Access Controls**
+ *
+ * .. code-block:: gyro
+ *
+ *    aclAll: $(external-query google::acl)
+ *
+ *
+ * **A single Bucket Access Controls for the bucket named "acl-example" and the entity value "domain-sample.com"**
+ *
+ * .. code-block:: gyro
+ *
+ *    acl: $(external-query google::acl {bucket: 'acl-example', entity: 'domain-sample.com'})
+ */
+@Type("acl")
+public class BucketAccessControlFinder extends GoogleFinder<Storage, BucketAccessControl, BucketAccessControlResource> {
+
+    private BucketResource bucket;
+    private String entity;
+
+    /**
+     * Name of a bucket.
+     */
+    public BucketResource getBucket() {
+        return bucket;
+    }
+
+    public void setBucket(BucketResource bucket) {
+        this.bucket = bucket;
+    }
+
+    /**
+     * Entity value holding the permission.
+     */
+    public String getEntity() {
+        return entity;
+    }
+
+    public void setEntity(String entity) {
+        this.entity = entity;
+    }
+
+    @Override
+    protected List<BucketAccessControl> findAllGoogle(Storage client) {
+        List<BucketAccessControl> acls = new ArrayList<>();
+        String pageToken;
+
+        try {
+            do {
+                Buckets results = client.buckets().list(getProjectId()).execute();
+                pageToken = results.getNextPageToken();
+
+                if (results.getItems() != null) {
+                    Iterator<Bucket> it = results.getItems().iterator();
+                    while (it.hasNext()) {
+                        Bucket b = it.next();
+                        List<BucketAccessControl> items = client.bucketAccessControls().list(b.getName()).execute().getItems();
+                        if (items != null) {
+                            acls.addAll(items);
+                        }
+                    }
+                }
+            } while (pageToken != null);
+        } catch (IOException e) {
+            throw new GyroException(e);
+        }
+
+        return acls;
+    }
+
+    @Override
+    protected List<BucketAccessControl> findGoogle(Storage client, Map<String, String> filters) {
+
+        if (filters.containsKey("bucket") && filters.containsKey("entity")) {
+            try {
+                BucketAccessControl acl = client.bucketAccessControls().get(filters.get("bucket"), filters.get("entity")).execute();
+
+                if (acl != null) {
+                    return Collections.singletonList(acl);
+                }
+            } catch (IOException e) {
+                throw new GyroException(e);
+            }
+        }
+
+        return Collections.emptyList();
+    }
 }
--- src/main/java/gyro/google/storage/BucketAccessControlResource.java
+++ src/main/java/gyro/google/storage/BucketAccessControlResource.java
@@ -16,5 +16,277 @@
 
 package gyro.google.storage;
 
-public class BucketAccessControlResource {
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.services.storage.Storage;
+import com.google.api.services.storage.model.BucketAccessControl;
+import gyro.core.GyroException;
+import gyro.core.GyroUI;
+import gyro.core.Type;
+import gyro.core.resource.Id;
+import gyro.core.resource.Resource;
+import gyro.core.resource.Updatable;
+import gyro.core.scope.State;
+import gyro.core.validation.Regex;
+import gyro.core.validation.Required;
+import gyro.core.validation.ValidStrings;
+import gyro.google.Copyable;
+import gyro.google.GoogleResource;
+
+import java.io.IOException;
+import java.util.Set;
+
+/**
+ *
+ * Example
+ * -------
+ *
+ * .. code-block:: gyro
+ *
+ *     google::bucket 'acl-bucket'
+ *         name: 'acl-example'
+ *         location: 'US-CENTRAL1'
+ *     end
+ *
+ *     google::acl 'acl-one'
+ *        bucket: $(google::bucket acl-bucket)
+ *        entity: 'domain-brightspot.com'
+ *        role: 'OWNER'
+ *     end
+ *
+ *     google::acl 'acl-two'
+ *        bucket: $(google::bucket acl-bucket)
+ *        entity: 'domain-sample.com'
+ *        role: 'READER'
+ *     end
+ */
+@Type("acl")
+public class BucketAccessControlResource extends GoogleResource implements Copyable<BucketAccessControl> {
+    
+    private static final String ENTITY_REGEX = "\\b(allUsers|allAuthenticatedUsers)\\b|^(user|group|domain|project)-.*?";
+
+    private BucketResource bucket;
+    private String role;
+    private String entity;
+    private String userProject;
+    private String id;
+    private String selfLink;
+    private String domain;
+    private String email;
+    private String entityId;
+    private BucketAccessControlProjectTeam projectTeam;
+
+    /**
+     * The associated Bucket.
+     */
+    public BucketResource getBucket() {
+        return bucket;
+    }
+
+    public void setBucket(BucketResource bucket) {
+        this.bucket = bucket;
+    }
+    
+    /**
+     * The access permission for the entity. Valid values are ``OWNER``, ``READER``, or ``WRITER``.
+     */
+    @Required
+    @Updatable
+    @ValidStrings({"OWNER", "READER", "WRITER"})
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    /**
+     * The entity holding the permission, in one of the following forms ``user-<userId>``, ``user-<email>``, ``group-<groupId>``, ``group-<email>``, ``domain-<domain>``, ``project-<team-projectId>``, ``allUsers``, or ``allAuthenticatedUsers``.
+     */
+    @Regex(ENTITY_REGEX)
+    @Required
+    @Updatable
+    public String getEntity() {
+        return entity;
+    }
+
+    public void setEntity(String entity) {
+        this.entity = entity;
+    }
+
+    /**
+     * The project to be billed for this request.
+     */
+    @Updatable
+    public String getUserProject() {
+        return userProject;
+    }
+
+    public void setUserProject(String userProject) {
+        this.userProject = userProject;
+    }
+
+    /**
+     * The domain associated with the entity.
+     */
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    /**
+     * Unique ID for the resource.
+     */
+    @Id
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    /**
+     * The link to this access-control entry.
+     */
+    public String getSelfLink() {
+        return selfLink;
+    }
+
+    public void setSelfLink(String selfLink) {
+        this.selfLink = selfLink;
+    }
+    
+    /**
+     * The email address associated with the entity.
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * The ID for the entity.
+     */
+    public String getEntityId() {
+        return entityId;
+    }
+
+    public void setEntityId(String entityId) {
+        this.entityId = entityId;
+    }
+
+    /**
+     * The project team associated with the entity.
+     */
+    public BucketAccessControlProjectTeam getProjectTeam() {
+        return projectTeam;
+    }
+
+    public void setProjectTeam(BucketAccessControlProjectTeam projectTeam) {
+        this.projectTeam = projectTeam;
+    }
+
+    @Override
+    public boolean refresh() {
+        Storage storage = createClient(Storage.class);
+
+        try {
+            BucketAccessControl acl = storage.bucketAccessControls()
+                    .get(getBucket().getName(), getEntity())
+                    .setUserProject(getUserProject())
+                    .execute();
+
+            if (acl == null) {
+                return false;
+            }
+
+            copyFrom(acl);
+
+            return true;
+        } catch (GoogleJsonResponseException e) {
+            if (e.getDetails().getCode() == 404) {
+                return false;
+            } else {
+                throw new GyroException(e.getDetails().getMessage());
+            }
+        } catch (IOException e) {
+            throw new GyroException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void create(GyroUI ui, State state) throws Exception {
+        Storage storage = createClient(Storage.class);
+
+        BucketAccessControl acl = new BucketAccessControl();
+        acl.setBucket(getBucket().getName());
+        acl.setEntity(getEntity());
+        acl.setRole(getRole());
+
+        try {
+            copyFrom(storage.bucketAccessControls().insert(getBucket().getName(), acl).execute());
+        } catch (GoogleJsonResponseException e) {
+            throw new GyroException(e.getDetails().getMessage());
+        } catch (Exception e) {
+            throw new GyroException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void update(GyroUI ui, State state, Resource current, Set<String> changedFieldNames) throws Exception {
+        Storage storage = createClient(Storage.class);
+        BucketAccessControl acl = new BucketAccessControl();
+
+        if (changedFieldNames.contains("bucket")) {
+            acl.setRole(getBucket().getName());
+        }
+
+        if (changedFieldNames.contains("entity")) {
+            acl.setEntity(getEntity());
+        }
+
+        if (changedFieldNames.contains("role")) {
+            acl.setRole(getRole());
+        }
+
+        try {
+            copyFrom(storage.bucketAccessControls().patch(getBucket().getName(), getEntity(), acl).execute());
+        } catch (GoogleJsonResponseException e) {
+            throw new GyroException(e.getDetails().getMessage());
+        }
+    }
+
+    @Override
+    public void delete(GyroUI ui, State state) throws Exception {
+        Storage storage = createClient(Storage.class);
+
+        try {
+            storage.bucketAccessControls().delete(getBucket().getName(), getEntity()).execute();
+        } catch (GoogleJsonResponseException e) {
+            throw new GyroException(e.getDetails().getMessage());
+        }
+    }
+
+    @Override
+    public void copyFrom(BucketAccessControl model) {
+        setRole(model.getRole());
+        setId(model.getId());
+        setSelfLink(model.getSelfLink());
+        setDomain(model.getDomain());
+        setEmail(model.getEmail());
+        setEntityId(model.getEntityId());
+
+        if (model.getProjectTeam() != null) {
+            BucketAccessControlProjectTeam projectTeam = newSubresource(BucketAccessControlProjectTeam.class);
+            projectTeam.copyFrom(model.getProjectTeam());
+            setProjectTeam(projectTeam);
+        }
+    }
 }
