110c2cd09cb934f0a675d09d28412652d86ba8e7
Andrew Sung <asung@perfectsensedigital.com>
2019-11-27 10:57:55 -0500

    Adds DNS resource record set support.

---
+++ src/main/java/gyro/google/dns/ResourceRecordSetFinder.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2019, Perfect Sense, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package gyro.google.dns;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.services.dns.Dns;
+import com.google.api.services.dns.model.ResourceRecordSet;
+import com.google.api.services.dns.model.ResourceRecordSetsListResponse;
+import gyro.core.GyroException;
+import gyro.core.Type;
+import gyro.google.GoogleFinder;
+
+/**
+ * Query resouce record set.
+ *
+ * Example
+ * -------
+ *
+ * .. code-block:: gyro
+ *
+ *    resource-record-set: $(external-query google::dns-resource-record-set { name: 'managed-zone-example-private' })
+ */
+@Type("dns-resource-record-set")
+public class ResourceRecordSetFinder extends GoogleFinder<Dns, ResourceRecordSet, ResourceRecordSetResource> {
+
+    private String name;
+
+    /**
+     * User assigned name for this resource. Must be unique within the project. The name must be 1-63 characters long,
+     * must begin with a letter, end with a letter or digit, and only contain lowercase letters, digits or dashes.
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected List<ResourceRecordSet> findAllGoogle(Dns client) {
+        // TODO: return all resource record sets through out all zones?
+        return Collections.emptyList();
+    }
+
+    @Override
+    protected List<ResourceRecordSet> findGoogle(Dns client, Map<String, String> filters) {
+        List<ResourceRecordSet> allRecordSets = new ArrayList<>();
+        try {
+            Dns.ResourceRecordSets.List request = client.resourceRecordSets().list(getProjectId(), filters.get("name"));
+            String nextPageToken = null;
+
+            do {
+                ResourceRecordSetsListResponse response = request.execute();
+                allRecordSets.addAll(response.getRrsets());
+                nextPageToken = response.getNextPageToken();
+                request.setPageToken(nextPageToken);
+            } while (nextPageToken != null);
+        } catch (GoogleJsonResponseException je) {
+            throw new GyroException(je.getDetails().getMessage());
+        } catch (IOException ex) {
+            throw new GyroException(ex);
+        }
+        return allRecordSets;
+    }
+}
---
+++ src/main/java/gyro/google/dns/ResourceRecordSetResource.java
@@ -0,0 +1,248 @@
+/*
+ * Copyright 2019, Perfect Sense, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package gyro.google.dns;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.services.dns.Dns;
+import com.google.api.services.dns.model.Change;
+import com.google.api.services.dns.model.ResourceRecordSet;
+import com.google.api.services.dns.model.ResourceRecordSetsListResponse;
+import gyro.core.GyroException;
+import gyro.core.GyroUI;
+import gyro.core.Type;
+import gyro.core.resource.Resource;
+import gyro.core.resource.Updatable;
+import gyro.core.scope.State;
+import gyro.core.validation.Required;
+import gyro.google.Copyable;
+import gyro.google.GoogleResource;
+import gyro.google.Requestable;
+
+/**
+ * Creates a Resource Record Set.
+ *
+ * Example
+ * -------
+ *
+ * .. code-block:: gyro
+ *
+ *     google::dns-resource-record-set managed-zone-example-public-www-cname
+ *         managed-zone: $(google::dns-managed-zone managed-zone-example-public)
+ *
+ *         name: "www.p.example.com."
+ *         type: "CNAME"
+ *         rrdatas: [ "fe.p.example.com." ]
+ *     end
+ *
+ *     google::dns-resource-record-set managed-zone-example-public-fe-a
+ *         managed-zone: $(google::dns-managed-zone managed-zone-example-public)
+ *
+ *         name: "fe.p.example.com."
+ *         type: "A"
+ *         rrdatas: [ "192.168.1.156", "192.168.1.158" ]
+ *     end
+ */
+@Type("dns-resource-record-set")
+public class ResourceRecordSetResource extends GoogleResource
+    implements Copyable<ResourceRecordSet>, Requestable<ResourceRecordSet> {
+
+    private ManagedZoneResource managedZone;
+
+    /**
+     * For example, www.example.com. The value may be ``null``.
+     */
+    private String name;
+
+    /**
+     * As defined in RFC 1035 (section 5) and RFC 1034 (section 3.6.1) -- see examples. The value may be ``null``.
+     */
+    private List<String> rrdatas;
+
+    /**
+     * As defined in RFC 4034 (section 3.2). The value may be ``null``.
+     */
+    private List<String> signatureRrdatas;
+
+    /**
+     * Number of seconds that this ResourceRecordSet can be cached by resolvers. The value may be ``null``.
+     */
+    private Integer ttl;
+
+    /**
+     * The identifier of a supported record type. See the list of Supported DNS record types.
+     * The value may be ``null``.
+     */
+    private String type;
+
+    @Required
+    public ManagedZoneResource getManagedZone() {
+        return managedZone;
+    }
+
+    public void setManagedZone(ManagedZoneResource managedZone) {
+        this.managedZone = managedZone;
+    }
+
+    @Updatable
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Updatable
+    public List<String> getRrdatas() {
+        return rrdatas;
+    }
+
+    public void setRrdatas(List<String> rrdatas) {
+        this.rrdatas = rrdatas;
+    }
+
+    @Updatable
+    public List<String> getSignatureRrdatas() {
+        return signatureRrdatas;
+    }
+
+    public void setSignatureRrdatas(List<String> signatureRrdatas) {
+        this.signatureRrdatas = signatureRrdatas;
+    }
+
+    @Updatable
+    public Integer getTtl() {
+        return ttl;
+    }
+
+    public void setTtl(Integer ttl) {
+        this.ttl = ttl;
+    }
+
+    @Updatable
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean refresh() {
+        Dns client = createClient(Dns.class);
+        String managedZoneName = getManagedZone().getName();
+
+        try {
+            Dns.ResourceRecordSets.List request = client.resourceRecordSets().list(getProjectId(), managedZoneName);
+            request.setName(getName());
+            request.setType(getType());
+            ResourceRecordSetsListResponse response = request.execute();
+            List<ResourceRecordSet> rrsets = response.getRrsets();
+
+            if (rrsets.isEmpty()) {
+                return false;
+            } else if (rrsets.size() > 1) {
+                throw new GyroException(
+                    String.format("Multiple records found! [%s] [%s] [%s]", managedZoneName, getName(), getType()));
+            }
+            copyFrom(rrsets.get(0));
+            return true;
+        } catch (GoogleJsonResponseException je) {
+            if (je.getDetails().getMessage().matches("The resource (.*) was not found")) {
+                return false;
+            } else {
+                throw new GyroException(je.getDetails().getMessage());
+            }
+        } catch (Exception ex) {
+            throw new GyroException(ex.getMessage(), ex.getCause());
+        }
+    }
+
+    @Override
+    public void create(GyroUI ui, State state) throws Exception {
+        Change change = new Change();
+        change.setAdditions(Collections.singletonList(copyTo()));
+        process(ui, change);
+    }
+
+    @Override
+    public void update(
+        GyroUI ui, State state, Resource current, Set<String> changedFieldNames) throws Exception {
+
+        if (!(current instanceof ResourceRecordSetResource)) {
+            throw new GyroException("Incompatible resource type! " + current.getClass().getName());
+        }
+        Change change = new Change();
+        change.setDeletions(Collections.singletonList(((ResourceRecordSetResource) current).copyTo()));
+        change.setAdditions(Collections.singletonList(copyTo()));
+        process(ui, change);
+    }
+
+    @Override
+    public void delete(GyroUI ui, State state) throws Exception {
+        Change change = new Change();
+        change.setDeletions(Collections.singletonList(copyTo()));
+        process(ui, change);
+    }
+
+    @Override
+    public void copyFrom(ResourceRecordSet model) {
+        setName(model.getName());
+        setRrdatas(model.getRrdatas());
+        setSignatureRrdatas(model.getSignatureRrdatas());
+        setTtl(model.getTtl());
+        setType(model.getType());
+    }
+
+    @Override
+    public ResourceRecordSet copyTo() {
+        ResourceRecordSet resourceRecordSet = new ResourceRecordSet();
+        resourceRecordSet.setName(getName());
+        resourceRecordSet.setRrdatas(getRrdatas());
+        resourceRecordSet.setSignatureRrdatas(getSignatureRrdatas());
+        resourceRecordSet.setTtl(getTtl());
+        resourceRecordSet.setType(getType());
+        return resourceRecordSet;
+    }
+
+    private void process(GyroUI ui, Change change) {
+        Dns client = createClient(Dns.class);
+        try {
+            Change response = client.changes().create(getProjectId(), getManagedZone().getName(), change).execute();
+            Dns.Changes.Get getRequest = client
+                .changes()
+                .get(getProjectId(), getManagedZone().getName(), response.getId());
+
+            // TODO: limit retry?
+            long count = 1;
+
+            while (response.getStatus().equals("pending")) {
+                ui.write("\nWaiting to be updated.");
+                Thread.sleep(1000L * count++);
+                response = getRequest.execute();
+            }
+            refresh();
+        } catch (Exception ex) {
+            throw new GyroException(ex.getMessage(), ex.getCause());
+        }
+    }
+}
