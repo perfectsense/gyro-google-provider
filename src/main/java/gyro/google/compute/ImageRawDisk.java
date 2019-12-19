b6e6de776069e26819ce1357b5249f13d0a7d7f6
alex-lambrides <alex.lambrides@perfectsensedigital.com>
2019-12-19 15:15:38 -0500

    Change raw disk to use string url from incorrect Bucket resource.

--- src/main/java/gyro/google/compute/ImageRawDisk.java
+++ src/main/java/gyro/google/compute/ImageRawDisk.java
@@ -20,37 +20,33 @@ import com.google.api.services.compute.model.Image;
 import gyro.core.resource.Diffable;
 import gyro.core.validation.Required;
 import gyro.google.Copyable;
-import gyro.google.storage.BucketResource;
-import org.apache.commons.lang3.StringUtils;
 
 /**
  * A raw disk file used to create an image.
  */
 public class ImageRawDisk extends Diffable implements Copyable<Image.RawDisk> {
 
-    private BucketResource source;
+    private String source;
 
     /**
-     * The storage file that should be the source of the image. File must use ``.tar.gz`` extension and the file inside the archive must be named ``disk.raw``. See `Manually importing virtual disks  <https://cloud.google.com/compute/docs/import/import-existing-image>`_ for instructions on how to create the file. (Required)
+     *  The full Google Cloud Storage URL of the storage file that should be the source of the image. File must use ``.tar.gz`` extension and the file inside the archive must be named ``disk.raw``. See `Manually importing virtual disks  <https://cloud.google.com/compute/docs/import/import-existing-image>`_ for instructions on how to create the file. (Required)
      */
     @Required
-    public BucketResource getSource() {
+    public String getSource() {
         return source;
     }
 
-    public void setSource(BucketResource source) {
+    public void setSource(String source) {
         this.source = source;
     }
 
     @Override
     public void copyFrom(Image.RawDisk model) {
-        if (StringUtils.isNotBlank(model.getSource())) {
-            setSource(findById(BucketResource.class, model.getSource()));
-        }
+        setSource(model.getSource());
     }
 
     Image.RawDisk toRawDisk() {
         return new Image.RawDisk()
-            .setSource(getSource().getSelfLink());
+            .setSource(getSource());
     }
 }
