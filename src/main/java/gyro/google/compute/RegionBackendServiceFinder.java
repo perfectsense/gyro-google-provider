61f105bce956752073a0ec83e1164a8dedaf67b7
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-12-19 19:25:49 -0500

    Add regional backend service finder

---
+++ src/main/java/gyro/google/compute/RegionBackendServiceFinder.java
@@ -0,0 +1,93 @@
+package gyro.google.compute;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.google.api.services.compute.Compute;
+import com.google.api.services.compute.model.BackendService;
+import com.google.api.services.compute.model.BackendServiceAggregatedList;
+import com.google.api.services.compute.model.BackendServicesScopedList;
+import gyro.core.Type;
+import gyro.google.GoogleFinder;
+
+/**
+ * Query region backend service.
+ *
+ * Example
+ * -------
+ *
+ * .. code-block:: gyro
+ *
+ *    compute-region-backend-service: $(external-query google::compute-region-backend-service { name: 'compute-region-backend-service-example'})
+ */
+@Type("compute-region-backend-service")
+public class RegionBackendServiceFinder extends GoogleFinder<Compute, BackendService, RegionBackendServiceResource> {
+
+    private String region;
+    private String name;
+
+    /**
+     * The name of the backend service.
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * The region of the backend service.
+     */
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    @Override
+    protected List<BackendService> findAllGoogle(Compute client) throws Exception {
+        List<BackendService> backendServices = new ArrayList<>();
+        BackendServiceAggregatedList backendServiceList;
+        String nextPageToken = null;
+
+        do {
+            backendServiceList = client.backendServices().aggregatedList(getProjectId()).execute();
+            backendServices.addAll(backendServiceList
+                .getItems().values().stream()
+                .map(BackendServicesScopedList::getBackendServices)
+                .filter(Objects::nonNull)
+                .flatMap(Collection::stream)
+                .filter(backendService -> backendService.getRegion() != null)
+                .collect(Collectors.toList()));
+            nextPageToken = backendServiceList.getNextPageToken();
+        } while (nextPageToken != null);
+
+        return backendServices;
+    }
+
+    @Override
+    protected List<BackendService> findGoogle(
+        Compute client, Map<String, String> filters) throws Exception {
+        if (filters.containsKey("name")) {
+            return Collections.singletonList(client.regionBackendServices()
+                .get(getProjectId(), filters.get("region"), filters.get("name"))
+                .execute());
+        } else {
+            return Optional.ofNullable(client.regionBackendServices()
+                .list(getProjectId(), filters.get("region"))
+                .execute()
+                .getItems())
+                .orElse(new ArrayList<>());
+        }
+    }
+}
