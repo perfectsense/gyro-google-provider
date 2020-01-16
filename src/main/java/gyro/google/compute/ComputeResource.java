ad5c248150ff9b7db33de14b279499d7e6843e97
Andrew Sung <asung@perfectsensedigital.com>
2020-01-16 11:59:57 -0500

    Fixes `waitForResponse` API to make more flexible.

--- src/main/java/gyro/google/compute/ComputeResource.java
+++ src/main/java/gyro/google/compute/ComputeResource.java
@@ -22,44 +22,58 @@ import java.util.stream.Collectors;
 import com.google.api.services.compute.Compute;
 import com.google.api.services.compute.model.Operation;
 import gyro.core.GyroException;
-import gyro.core.Wait;
+import gyro.core.Waiter;
 import gyro.google.GoogleResource;
 
 public abstract class ComputeResource extends GoogleResource {
 
-    public void waitForCompletion(Compute compute, Operation operation) {
-        Wait.atMost(60, TimeUnit.SECONDS)
-            .until(() -> {
-                    Operation response = null;
-                    String zone = operation.getZone();
+    private static final long DEFAULT_WAIT_DURATION = 1L;
 
-                    if (zone != null) {
-                        String[] bits = zone.split("/");
-                        zone = bits[bits.length - 1];
-                        Compute.ZoneOperations.Get get = compute.zoneOperations()
-                            .get(getProjectId(), zone, operation.getName());
+    private static final TimeUnit DEFAULT_WAIT_TIME_UNIT = TimeUnit.MINUTES;
+
+    public void waitForCompletion(Compute compute, Operation operation) {
+        waitForCompletion(compute, operation, 0, null);
+    }
+
+    public void waitForCompletion(Compute compute, Operation operation, long duration, TimeUnit unit) {
+        Waiter waiter = new Waiter();
+
+        if (duration > 0 && unit != null) {
+            waiter.atMost(duration, unit);
+        } else {
+            waiter.atMost(DEFAULT_WAIT_DURATION, DEFAULT_WAIT_TIME_UNIT);
+        }
+        waiter.until(() -> {
+                Operation response = null;
+                String zone = operation.getZone();
+
+                if (zone != null) {
+                    String[] bits = zone.split("/");
+                    zone = bits[bits.length - 1];
+                    Compute.ZoneOperations.Get get = compute.zoneOperations()
+                        .get(getProjectId(), zone, operation.getName());
+                    response = get.execute();
+                } else {
+                    String region = operation.getRegion();
+
+                    if (region != null) {
+                        region = region.substring(region.lastIndexOf("/") + 1);
+                        Compute.RegionOperations.Get get = compute.regionOperations()
+                            .get(getProjectId(), region, operation.getName());
                         response = get.execute();
                     } else {
-                        String region = operation.getRegion();
-
-                        if (region != null) {
-                            region = region.substring(region.lastIndexOf("/") + 1);
-                            Compute.RegionOperations.Get get = compute.regionOperations()
-                                .get(getProjectId(), region, operation.getName());
-                            response = get.execute();
-                        } else {
-                            Compute.GlobalOperations.Get get = compute.globalOperations()
-                                .get(getProjectId(), operation.getName());
-                            response = get.execute();
-                        }
+                        Compute.GlobalOperations.Get get = compute.globalOperations()
+                            .get(getProjectId(), operation.getName());
+                        response = get.execute();
                     }
-
-                    if (response != null && response.getError() != null) {
-                        throw new GyroException(formatOperationErrorMessage(response.getError()));
-                    }
-                    return response != null && response.getStatus().equals("DONE");
                 }
-            );
+
+                if (response != null && response.getError() != null) {
+                    throw new GyroException(formatOperationErrorMessage(response.getError()));
+                }
+                return response != null && response.getStatus().equals("DONE");
+            }
+        );
     }
 
     protected static String formatOperationErrorMessage(Operation.Error error) {
