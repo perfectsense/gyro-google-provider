13a4e47ce283b7467040cec201c0578910c357c8
John Pencola <paulpencola@gmail.com>
2020-01-17 15:34:54 -0500

    Change id to use selfLink field.

--- src/main/java/gyro/google/compute/SecurityPolicyResource.java
+++ src/main/java/gyro/google/compute/SecurityPolicyResource.java
@@ -77,7 +77,6 @@ public class SecurityPolicyResource extends ComputeResource implements Copyable<
     /**
      * The name of the security policy. The name must be 1-63 characters long and the first character must be a lowercase letter, and all following characters must be a dash, lowercase letter, or digit, except the last character, which cannot be a dash. (Required)
      */
-    @Id
     @Required
     @Regex("(?:(?:[-a-z0-9]{1,63}\\.)*(?:[a-z](?:[-a-z0-9]{0,61}[a-z0-9])?):)?(?:[0-9]{1,19}|(?:[a-z0-9](?:[-a-z0-9]{0,61}[a-z0-9])?))")
     public String getName() {
@@ -103,6 +102,7 @@ public class SecurityPolicyResource extends ComputeResource implements Copyable<
     /**
      * The fully-qualified URL linking back to the security policy.
      */
+    @Id
     @Output
     public String getSelfLink() {
         return selfLink;
