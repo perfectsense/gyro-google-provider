aded87439aee1cdcbd3ee3e9e1f53fa82a397a8f
Andrew Sung <asung@perfectsensedigital.com>
2020-02-03 15:55:53 -0500

    Revert "Removes unused file."
    This reverts commit dbf281d1f24a5ad2c60d3a424502f34b4369c992.

---
+++ src/main/java/gyro/google/compute/ComputeInstanceGroupManagerStatusVersionTarget.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2020, Perfect Sense, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package gyro.google.compute;
+
+import gyro.core.resource.Diffable;
+import gyro.core.resource.Output;
+
+public class ComputeInstanceGroupManagerStatusVersionTarget extends Diffable {
+
+    private Boolean isReached;
+
+    /**
+     * A bit indicating whether version target has been reached in this managed instance group, i.e. all instances are in their target version. Instances' target version are specified by version field on Instance Group Manager.
+     */
+    @Output
+    public Boolean getIsReached() {
+        return isReached;
+    }
+
+    public void setIsReached(Boolean isReached) {
+        this.isReached = isReached;
+    }
+
+    @Override
+    public String primaryKey() {
+        // TODO: implement
+        return "";
+    }
+}
