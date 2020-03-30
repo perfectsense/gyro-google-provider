54b92c6375fe47fb601035e737aa9500db62191b
Harshil Jain <harjain99@gmail.com>
2020-03-30 11:24:08 -0400

    Add filters to Service Account Finder

--- src/main/java/gyro/google/iam/ServiceAccountFinder.java
+++ src/main/java/gyro/google/iam/ServiceAccountFinder.java
@@ -2,11 +2,13 @@ package gyro.google.iam;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.google.api.services.iam.v1.Iam;
 import com.google.api.services.iam.v1.model.ServiceAccount;
 import gyro.core.Type;
 import gyro.google.GoogleFinder;
+import gyro.google.util.Utils;
 
 /**
  * Query service account.
@@ -21,6 +23,25 @@ import gyro.google.GoogleFinder;
 @Type("service-account")
 public class ServiceAccountFinder extends GoogleFinder<Iam, ServiceAccount, ServiceAccountResource> {
 
+    private String name;
+    private String displayName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
     @Override
     protected List<ServiceAccount> findAllGoogle(Iam client) throws Exception {
         return client.projects().serviceAccounts().list("projects/" + getProjectId()).execute().getAccounts();
@@ -28,6 +49,24 @@ public class ServiceAccountFinder extends GoogleFinder<Iam, ServiceAccount, Serv
 
     @Override
     protected List<ServiceAccount> findGoogle(Iam client, Map<String, String> filters) throws Exception {
-        throw new UnsupportedOperationException("Finding `service-accounts` with filters is not supported!!");
+        List<ServiceAccount> accounts = client.projects()
+            .serviceAccounts()
+            .list("projects/" + getProjectId())
+            .execute()
+            .getAccounts();
+
+        if (filters.containsKey("name")) {
+            accounts = accounts.stream()
+                .filter(a -> Utils.getServiceAccountNameFromId(a.getName()).equals(filters.get("name")))
+                .collect(
+                    Collectors.toList());
+        }
+
+        if (filters.containsKey("display-name")) {
+            accounts = accounts.stream()
+                .filter(a -> a.getDisplayName().equals(filters.get("display-name"))).collect(Collectors.toList());
+        }
+
+        return accounts;
     }
 }
